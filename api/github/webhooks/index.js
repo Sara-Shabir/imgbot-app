f480da382c597cd452f125ac0c40387ae9d8e022
Sara Shabir <sarashabir56@gmail.com>
2026-08-27 10:49:11 +0500

    Update index.js

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,12 +1,1 @@
-// Vercel deploys everything under /api as its own serverless function.
-// This file turns the Probot app into a plain (req, res) handler so it can
-// run without a persistent server.
-const { createNodeMiddleware, createProbot } = require("probot");
-const app = require("../../../src/index.js");
-
-const probot = createProbot();
-
-module.exports = createNodeMiddleware(app, {
-  probot,
-  webhooksPath: "/api/github/webhooks",
-});
+module.exports = require("../../../index.js");
